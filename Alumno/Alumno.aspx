44d27ebd7bd80820b893df6d5e8728bf4142ef5c
Steven <stevengutaven@gmail.com>
2020-03-14 00:03:00 -0600

    Clase actualizada de Alumno

---
+++ Alumno/Alumno.aspx
@@ -0,0 +1,103 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Alumno.aspx.cs" Inherits="FundaVida.Alumno" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <br />
+            <asp:TextBox ID="txtCedula" runat="server"></asp:TextBox>
+            <asp:Label ID="Label1" runat="server" Text="Cedula"></asp:Label>
+            <br />
+            <asp:CheckBox ID="cbActivo" runat="server" />
+            <asp:Label ID="Label2" runat="server" Text="Activo"></asp:Label>
+            <br />
+            <br />
+            <asp:TextBox ID="txtInstitucion" runat="server"></asp:TextBox>
+            <asp:Label ID="Label3" runat="server" Text="Institución"></asp:Label>
+            <asp:Button ID="Button1" runat="server" Text="Button" />
+            <br />
+            <asp:DropDownList ID="ddGrado" runat="server">
+                <asp:ListItem Value="1">Octavo</asp:ListItem>
+                <asp:ListItem Value="2">Noveno</asp:ListItem>
+            </asp:DropDownList>
+            <asp:Label ID="Label4" runat="server" Text="Grado"></asp:Label>
+            <br />
+            <asp:DropDownList ID="DropDownList1" runat="server">
+            </asp:DropDownList>
+            <asp:Label ID="Label5" runat="server" Text="Encargado"></asp:Label>
+            <br />
+            <asp:CheckBox ID="cbRecogen" runat="server" />
+            <asp:Label ID="Label6" runat="server" Text="Lo recogen"></asp:Label>
+            <br />
+            <asp:DropDownList ID="ddSede" runat="server">
+                <asp:ListItem Value="1">Concepcion</asp:ListItem>
+                <asp:ListItem Value="2">25 julio</asp:ListItem>
+                <asp:ListItem Value="3">Linda Vista</asp:ListItem>
+            </asp:DropDownList>
+            <asp:Label ID="Label7" runat="server" Text="Sede"></asp:Label>
+            <br />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Label ID="lblValidar" runat="server" Text="."></asp:Label>
+            <br />
+            <br />
+            <asp:Button ID="btnIngresar" runat="server" OnClick="btnIngresar_Click" Text="Ingresar" />
+&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="btnRefrescar" runat="server" OnClick="btnRefrescar_Click" Text="Refrescar" />
+&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="btnEliminar" runat="server" OnClick="btnEliminar_Click" Text="Eliminar" />
+&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="btnEditar" runat="server" OnClick="btnEditar_Click" Text="Editar" />
+            <br />
+            <br />
+            <asp:Button ID="btnBuscar" runat="server" OnClick="btnBuscar_Click" Text="Buscar" />
+            <asp:TextBox ID="txtBuscar" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <asp:GridView ID="gVDatos" runat="server" BackColor="White" BorderColor="#999999" BorderStyle="Solid" BorderWidth="1px" CellPadding="3" ForeColor="Black" GridLines="Vertical">
+                <AlternatingRowStyle BackColor="#CCCCCC" />
+                <FooterStyle BackColor="#CCCCCC" />
+                <HeaderStyle BackColor="Black" Font-Bold="True" ForeColor="White" />
+                <PagerStyle BackColor="#999999" ForeColor="Black" HorizontalAlign="Center" />
+                <SelectedRowStyle BackColor="#000099" Font-Bold="True" ForeColor="White" />
+                <SortedAscendingCellStyle BackColor="#F1F1F1" />
+                <SortedAscendingHeaderStyle BackColor="#808080" />
+                <SortedDescendingCellStyle BackColor="#CAC9C9" />
+                <SortedDescendingHeaderStyle BackColor="#383838" />
+            </asp:GridView>
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+        </div>
+    </form>
+</body>
+</html>
---
+++ Alumno/Alumno.aspx.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace FundaVida
+{
+    public partial class Alumno : System.Web.UI.Page
+    {
+
+        #region "Variables globales"
+        SqlConnection cnx; //Declarando el objeto no lo inicializo
+        SqlCommand cmd; //Declarado
+        SqlDataReader dr;
+        SqlDataAdapter sda;
+        #endregion
+
+
+
+        #region "Metodos"
+
+        private bool establecerConexion()
+        {
+            try
+            {
+                cnx = new SqlConnection();
+                cnx.ConnectionString = ConfigurationManager.ConnectionStrings["conexionProduccion"].ConnectionString;
+                cnx.Open();
+                if (cnx.State == ConnectionState.Open)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+        }
+
+
+        public void ingresarAlumno()
+        {
+            
+                String query = "INSERT INTO Niño(Persona_idPersona, Activo , Institucion_idInstitucion, Grado_idGrado, Encargado_Persona_idPersona,Recogen, Cede_idCede )" +
+                  "VALUES(@Persona_idPersona, @Activo, @Institucion_idInstitucion, @Grado_idGrado, @Encargado_Persona_idPersona, @Recogen, @Cede_idCede )";
+                establecerConexion();
+                cmd = new SqlCommand(query, cnx);
+                cmd.Parameters.AddWithValue("@Persona_idPersona", Convert.ToInt32(txtCedula.Text));
+                cmd.Parameters.AddWithValue("@Activo", cbActivo.Checked);
+                cmd.Parameters.AddWithValue("@Institucion_idInstitucion", txtInstitucion.Text);
+                cmd.Parameters.AddWithValue("@Grado_idGrado", Convert.ToInt32(ddGrado.SelectedValue));
+                cmd.Parameters.AddWithValue("@Encargado_Persona_idPersona", Convert.ToInt32(ddEncargado.SelectedValue));
+                cmd.Parameters.AddWithValue("@Recogen", cbRecogen.Checked);
+                cmd.Parameters.AddWithValue("@Cede_idCede", Convert.ToInt32(ddSede.SelectedValue));
+
+                cmd.ExecuteNonQuery();
+
+
+        }
+
+        public void cargarAlumno()
+        {
+            try
+            {
+                establecerConexion();
+                SqlDataAdapter da = new SqlDataAdapter("SELECT P.idPersona, P.Nombre, P.Apellido1, P.Apellido2, N.Activo, N.Institucion_idInstitucion, N.Grado_idGrado, N.Encargado_Persona_idPersona N.Recogen, N.Cede_idCede FROM Niño N JOIN persona P on P.idPersona = N.Persona_idPersona", cnx);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                this.gVDatos.DataSource = dt;
+                gVDatos.DataBind();
+            }
+            catch (Exception e)
+            {
+                lblValidar.Text = e.ToString();
+                
+            }
+            
+        }
+
+        public void buscarAlumno()
+        {
+
+            
+                establecerConexion();
+                SqlDataAdapter da = new SqlDataAdapter("SELECT P.idPersona, P.Nombre, P.Apellido1, P.Apellido2, N.Activo, N.Institucion_idInstitucion, N.Grado_idGrado, N.Encargado_Persona_idPersona, N.Recogen, N.Cede_idCede FROM Niño N JOIN persona P on P.idPersona = N.Persona_idPersona where idPersona LIKE'" + txtBuscar.Text + "%'", cnx);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                this.gVDatos.DataSource = dt;
+                gVDatos.DataBind();
+
+      
+            
+
+        }
+
+        public void Eliminar()
+        {
+
+            try
+            {
+                String query = "DELETE FROM Niño WHERE Persona_idPersona = @Persona_idPersona";
+                establecerConexion();
+                cmd = new SqlCommand(query, cnx);
+                cmd.Parameters.AddWithValue("@Persona_idPersona", txtCedula.Text);
+                cmd.ExecuteNonQuery();
+                cnx.Close();
+
+            }
+            catch (Exception e)
+            {
+                lblValidar.Text = e.ToString();
+
+            }
+            
+        }
+
+        public void Editar()
+        {
+
+            try
+            {
+                String query = "UPDATE Niño SET  Activo = @Activo, Institucion_idInstitucion=@Institucion_idInstitucion, Grado_idGrado = @Grado_idGrado, Encargado_Persona_idPersona = @Encargado_Persona_idPersona, Recogen = @Recogen, Cede_idCede = @Cede_idCede  WHERE Persona_idPersona = @Persona_idPersona ";
+                establecerConexion();
+                cmd = new SqlCommand(query, cnx);
+                cmd.Parameters.AddWithValue("@Persona_idPersona", Convert.ToInt32(txtCedula.Text));
+                cmd.Parameters.AddWithValue("@Activo", cbActivo.Checked);
+                cmd.Parameters.AddWithValue("@Institucion_idInstitucion", txtInstitucion.Text);
+                cmd.Parameters.AddWithValue("@Grado_idGrado", Convert.ToInt32(ddGrado.SelectedValue));
+                cmd.Parameters.AddWithValue("@Encargado_Persona_idPersona", Convert.ToInt32(ddEncargado.SelectedValue));
+                cmd.Parameters.AddWithValue("@Recogen", cbRecogen.Checked);
+                cmd.Parameters.AddWithValue("@Cede_idCede", Convert.ToInt32(ddSede.SelectedValue));
+                cmd.ExecuteNonQuery();
+                cnx.Close();
+
+            }
+            catch (Exception e)
+            {
+
+                lblValidar.Text = e.ToString();
+            }
+            
+        }
+
+        public void seleccionarEncargado() {
+
+            String query = "Select Persona_idPersona from Encargado";
+            establecerConexion();
+            cmd = new SqlCommand(query, cnx);
+            SqlDataReader registro = cmd.ExecuteReader();
+            while (registro.Read())
+            {
+                ddEncargado.Items.Add(registro["Persona_idPersona"].ToString());
+            }
+
+        }
+
+        public void seleccionarAlumno()
+        {
+
+            String query = "Select idGrado from Grado";
+            establecerConexion();
+            cmd = new SqlCommand(query, cnx);
+            SqlDataReader registro = cmd.ExecuteReader();
+            while (registro.Read())
+            {
+                ddGrado.Items.Add(registro["idGrado"].ToString());
+            }
+
+        }
+
+
+        #endregion
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            seleccionarAlumno();
+            seleccionarEncargado();
+        }
+
+        protected void btnIngresar_Click(object sender, EventArgs e)
+        {
+            ingresarAlumno();
+        }
+
+        protected void btnRefrescar_Click(object sender, EventArgs e)
+        {
+            buscarAlumno();
+        }
+
+        protected void btnEliminar_Click(object sender, EventArgs e)
+        {
+            Eliminar();
+        }
+
+        protected void btnEditar_Click(object sender, EventArgs e)
+        {
+            Editar();
+        }
+
+        protected void btnBuscar_Click(object sender, EventArgs e)
+        {
+            buscarAlumno();
+        }
+
+        protected void ddSede_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
