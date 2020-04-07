03fe33caca14024d19cef31211d94acb03cc2dce
DavidCoto26 <61565188+DavidCoto26@users.noreply.github.com>
2020-04-06 21:01:59 -0600

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,62 @@
+﻿
+function succes() {
+	Swal.fire({
+		position: 'center',
+		icon: 'success',
+		title: 'Registo Ingresado con exito',
+		showConfirmButton: false,
+		timer: 1500
+	})
+
+}
+
+function succes2() {
+	Swal.fire({
+		position: 'center',
+		icon: 'success',
+		title: 'Registo Modificado con exito',
+		showConfirmButton: false,
+		timer: 1500
+	})
+
+}
+
+function succes3() {
+	Swal.fire({
+		position: 'center',
+		icon: 'success',
+		title: 'Registo Eliminado con exito',
+		showConfirmButton: false,
+		timer: 1500
+	})
+
+}
+
+function succes4() {
+	Swal.fire({
+		position: 'center',
+		icon: 'success',
+		title: 'Registo Encontrado con exito',
+		showConfirmButton: false,
+		timer: 1500
+	})
+
+}
+
+
+
+function error() {
+	Swal.fire({
+		position: 'center',
+		icon: 'info',
+		title: 'Error, vuelva a intentarlo',
+		showConfirmButton: false,
+		timer: 1500
+	})
+
+}
+
+
+
+
+
